bb2a14fda305fc6066ecd285885324926a2c26d5
davbadalyan <davit198820@gmail.com>
2021-04-14 10:05:24 +0400

    aaa

--- lesson1/lesson1.php
+++ lesson1/lesson1.php
@@ -1,58 +1,94 @@
 <?php 
 
-$newAge = 7;
+// $newAge = 7;
 
-class Animal{
-    public $age = 0;
-    public $createdAt;
-    protected $owner;
+// class Animal{
+//     public $age = 0;
+//     public $createdAt;
+//     protected $owner;
 
-    public function increaseAge(int $value)
-    {
-        $this->age += $value;
-    }
-    public function name() {
-        return true;
-    }
+//     public function increaseAge(int $value)
+//     {
+//         $this->age += $value;
+//     }
+//     public function name() {
+//         return true;
+//     }
     
-    public function getOwner() {
-        var_dump($this->owner);
-    }
+//     public function getOwner() {
+//         var_dump($this->owner);
+//     }
 
-    public function setAge ($newAge) {
-        if($newAge >= 0 && $newAge < 100) {
-            $this->age = $newAge;
+//     public function setAge ($newAge) {
+//         if($newAge >= 0 && $newAge < 100) {
+//             $this->age = $newAge;
+//         }
+//     }
+
+//     public function setOwner($newOwner) {
+//         if(!is_null($newOwner)) {
+//             $this->owner = $newOwner;
+//         }
+//     }
+
+//     public function init() {
+//         $this->createdAt = Date("d-m-Y");
+//     }
+    
+// }
+
+// $animal = new Animal();
+
+
+
+// var_dump($animal instanceof Animal);
+// var_dump($animal->age);
+// // $animal->age = "Error";
+// // $animal->increaseAge(4);
+// // if($newAge >= 0) {
+// //     $animal->age = $newAge;
+// // }
+// $animal->setAge($newAge);
+// // var_dump($animal->age);
+// // var_dump($animal->name());
+// $animal->setOwner("Hendo");
+// $animal->getOwner();
+// // var_dump($animal->owner);
+// $animal->init();
+// var_dump($animal->createdAt);
+
+
+
+
+
+class Product{
+    public $shipping;
+    public $currency;
+    protected $price;
+
+    protected $data = [
+        "Russia" => ["shipping" => false, "currency" => "RUB"],
+        "USA" => ["shipping" => true, "currency" => "$"],
+        "Armenia" => ["shipping" => true, "currency" => "AMD"]
+    ];
+
+    public function setPrice($newPrice) {
+        if(!is_null($newPrice)) {
+            $this->price = $newPrice;
         }
     }
 
-    public function setOwner($newOwner) {
-        if(!is_null($newOwner)) {
-            $this->owner = $newOwner;
-        }
+    public function getPrice() {
+        var_dump($this->price);
     }
 
-    public function init() {
-        $this->createdAt = Date("d-m-Y");
+    public function init($country) {
+        var_dump($this->data[$country]);
     }
-    
 }
 
-$animal = new Animal();
+$watch = new Product();
 
-
-
-var_dump($animal instanceof Animal);
-var_dump($animal->age);
-// $animal->age = "Error";
-// $animal->increaseAge(4);
-// if($newAge >= 0) {
-//     $animal->age = $newAge;
-// }
-$animal->setAge($newAge);
-// var_dump($animal->age);
-// var_dump($animal->name());
-$animal->setOwner("Hendo");
-$animal->getOwner();
-// var_dump($animal->owner);
-$animal->init();
-var_dump($animal->createdAt);
+$watch->setPrice("$300");
+// $watch->getPrice();
+$watch->init("USA");
